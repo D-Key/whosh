a8d2299b2b92aacbcb9f2228a633061d6adb7331
Dmitriy <dkomarov@gmail.com>
2012-11-14 15:43:30 -0500

    WPF: VS 2012 look-alike custom window chrome

---
+++ Whush Solution/Whush.Demo/Styles/CustomizedWindow/VS2012WindowStyle.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using System.Collections;
+
+namespace Whush.Demo.Styles.CustomizedWindow
+{
+    internal static class LocalExtensions
+    {
+        public static void ForWindowFromChild(this object childDependencyObject, Action<Window> action)
+        {
+            var element = childDependencyObject as DependencyObject;
+            while (element != null)
+            {
+                element = VisualTreeHelper.GetParent(element);
+                if (element is Window) { action(element as Window); break; }
+            }
+        }
+
+        public static void ForWindowFromTemplate(this object templateFrameworkElement, Action<Window> action)
+        {
+            Window window = ((FrameworkElement)templateFrameworkElement).TemplatedParent as Window;
+            if (window != null) action(window);
+        }
+
+        public static IntPtr GetWindowHandle(this Window window)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(window);
+            return helper.Handle;
+        }
+    }
+
+    public partial class VS2012WindowStyle
+    {
+        #region sizing event handlers
+
+        void OnSizeSouth(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.South); }
+        void OnSizeNorth(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.North); }
+        void OnSizeEast(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.East); }
+        void OnSizeWest(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.West); }
+        void OnSizeNorthWest(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.NorthWest); }
+        void OnSizeNorthEast(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.NorthEast); }
+        void OnSizeSouthEast(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.SouthEast); }
+        void OnSizeSouthWest(object sender, MouseButtonEventArgs e) { OnSize(sender, SizingAction.SouthWest); }
+
+        void OnSize(object sender, SizingAction action)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                sender.ForWindowFromTemplate(w =>
+                    {
+                        if (w.WindowState == WindowState.Normal)
+                            DragSize(w.GetWindowHandle(), action);
+                    });
+            }
+        }
+
+        void IconMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount > 1)
+            {
+                sender.ForWindowFromTemplate(w => w.Close());
+            }
+            else
+            {
+                sender.ForWindowFromTemplate(w =>
+                    SendMessage(w.GetWindowHandle(), WM_SYSCOMMAND, (IntPtr)SC_KEYMENU, (IntPtr)' '));
+            }
+        }
+
+        void CloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => w.Close());
+        }
+
+        void MinButtonClick(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => w.WindowState = WindowState.Minimized);
+        }
+
+        void MaxButtonClick(object sender, RoutedEventArgs e)
+        {
+            sender.ForWindowFromTemplate(w => w.WindowState = (w.WindowState == WindowState.Maximized) ? WindowState.Normal : WindowState.Maximized);
+        }
+
+        void TitleBarMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ClickCount > 1)
+            {
+                MaxButtonClick(sender, e);
+            }
+            else if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                sender.ForWindowFromTemplate(w => w.DragMove());
+            }
+        }
+
+        #endregion
+
+        #region P/Invoke
+
+        const int WM_SYSCOMMAND = 0x112;
+        const int SC_SIZE = 0xF000;
+        const int SC_KEYMENU = 0xF100;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        void DragSize(IntPtr handle, SizingAction sizingAction)
+        {
+            SendMessage(handle, WM_SYSCOMMAND, (IntPtr)(SC_SIZE + sizingAction), IntPtr.Zero);
+            SendMessage(handle, 514, IntPtr.Zero, IntPtr.Zero);
+        }
+
+        public enum SizingAction
+        {
+            North = 3,
+            South = 6,
+            East = 2,
+            West = 1,
+            NorthEast = 5,
+            NorthWest = 4,
+            SouthEast = 8,
+            SouthWest = 7
+        }
+
+        #endregion
+    }
+}
